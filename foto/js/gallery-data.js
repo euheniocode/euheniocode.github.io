8f975dea5761b206a4a6cfc811ea50647868863c
roboticseo <roboticseo@gmail.com>
2025-06-15 18:07:52 +0500

    Добавил photos

--- foto/js/gallery-data.js
+++ foto/js/gallery-data.js
@@ -1,11 +1,39 @@
-// Автоматически сгенерировано 2025-06-15 17:54
+// Автоматически сгенерировано 2025-06-15 18:07
 const galleryImages = [
   {
-    "src": "foto/images/dino.png",
+    "src": "foto/images/photo_2024-07-09_15-25-46.jpg",
     "caption": ""
   },
   {
-    "src": "foto/images/cactus.png",
+    "src": "foto/images/photo_2024-08-27_22-26-39.jpg",
+    "caption": ""
+  },
+  {
+    "src": "foto/images/photo_2024-11-23_13-50-23.jpg",
+    "caption": ""
+  },
+  {
+    "src": "foto/images/photo_2024-12-31_22-45-57.jpg",
+    "caption": ""
+  },
+  {
+    "src": "foto/images/photo_2025-06-15_18-06-19.jpg",
+    "caption": ""
+  },
+  {
+    "src": "foto/images/photo_2025-06-15_18-06-13.jpg",
+    "caption": ""
+  },
+  {
+    "src": "foto/images/photo_2024-12-31_22-46-53.jpg",
+    "caption": ""
+  },
+  {
+    "src": "foto/images/photo_2025-01-26_23-35-56.jpg",
+    "caption": ""
+  },
+  {
+    "src": "foto/images/photo_2025-02-08_15-46-38.jpg",
     "caption": ""
   }
 ];
