ad14407b773f834a43ed9623affe1093f16886f9
roboticseo <roboticseo@gmail.com>
2025-07-17 23:19:53 +0500

    Автоматизировал добавление картинок в галерею. Переписал Лехин скрипт

--- foto/js/gallery-data.js
+++ foto/js/gallery-data.js
@@ -1,39 +1,14 @@
-// Автоматически сгенерировано 2025-06-15 18:07
-const galleryImages = [
-  {
-    "src": "foto/images/photo_2024-07-09_15-25-46.jpg",
-    "caption": ""
-  },
-  {
-    "src": "foto/images/photo_2024-08-27_22-26-39.jpg",
-    "caption": ""
-  },
-  {
-    "src": "foto/images/photo_2024-11-23_13-50-23.jpg",
-    "caption": ""
-  },
-  {
-    "src": "foto/images/photo_2024-12-31_22-45-57.jpg",
-    "caption": ""
-  },
-  {
-    "src": "foto/images/photo_2025-06-15_18-06-19.jpg",
-    "caption": ""
-  },
-  {
-    "src": "foto/images/photo_2025-06-15_18-06-13.jpg",
-    "caption": ""
-  },
-  {
-    "src": "foto/images/photo_2024-12-31_22-46-53.jpg",
-    "caption": ""
-  },
-  {
-    "src": "foto/images/photo_2025-01-26_23-35-56.jpg",
-    "caption": ""
-  },
-  {
-    "src": "foto/images/photo_2025-02-08_15-46-38.jpg",
-    "caption": ""
-  }
-];
+// Автоматически сгенерировано 2025-07-17 23:18
+const images  = ['foto/images/photo_2025-07-17_23-17-10.jpg',
+'foto/images/photo_2025-07-17_23-16-44.jpg',
+'foto/images/photo_2025-07-17_23-16-27.jpg',
+'foto/images/photo_2025-07-17_23-15-41.jpg',
+'foto/images/photo_2025-06-15_18-06-19.jpg',
+'foto/images/photo_2025-06-15_18-06-13.jpg',
+'foto/images/photo_2025-02-08_15-46-38.jpg',
+'foto/images/photo_2025-01-26_23-35-56.jpg',
+'foto/images/photo_2024-12-31_22-46-53.jpg',
+'foto/images/photo_2024-12-31_22-45-57.jpg',
+'foto/images/photo_2024-11-23_13-50-23.jpg',
+'foto/images/photo_2024-08-27_22-26-39.jpg',
+'foto/images/photo_2024-07-09_15-25-46.jpg'];
